3eff6e9ac86003fe3710e274939e5bec3a93e781
rtuna <artur.hnat@gmail.com>
2022-11-05 21:58:40 +0100

    udpate logging

--- logic.go
+++ logic.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-// SubscribePositions subscribes to an user's potition details.
+// SubscribePositions subscribes to an user's potition details in a new goroutine.
 //
 // Returns two read-only channels, one with user's positions, other with any errors occured during the subsription.
 func (u *User) SubscribePositions(ctx context.Context) (<-chan Position, <-chan error) {
@@ -26,6 +26,7 @@ func (u *User) SubscribePositions(ctx context.Context) (<-chan Position, <-chan 
 				return
 
 			case <-t.C:
+				u.log.Printf("[%s] Checking for new positions\n", u.UID)
 				res, err := getOpenPositions(u.UID)
 				if err != nil {
 					ce <- fmt.Errorf("failed to fetch positions: %w", err)
@@ -37,7 +38,7 @@ func (u *User) SubscribePositions(ctx context.Context) (<-chan Position, <-chan 
 					continue
 				}
 
-				u.log.Printf("[%s] Updating %d positions, last updated at @ %d\n", u.UID, len(res.Data.OtherPositionRetList), res.Data.UpdateTimeStamp)
+				u.log.Printf("[%s] Updating %d positions\n", u.UID, len(res.Data.OtherPositionRetList))
 				u.handlePositions(res.Data.OtherPositionRetList, cp, ce)
 			}
 		}
@@ -68,17 +69,20 @@ func (u *User) handlePositions(rps []rawPosition, cp chan<- Position, ce chan<- 
 		if !ok {
 			// no previous position, so it's a new one
 			p.Type = Opened
+			u.log.Printf("[%s] New opened position: %s %f %s @ %f\n", u.UID, p.Direction, p.Amount, p.Ticker, p.EntryPrice)
 		} else if pp.Amount > p.Amount {
 			// previously saved amount is BIGGER than the current amount
 			// meaning the amount has DECREASED thus the position
 			// has been partially closed
 
+			u.log.Printf("[%s] Position partially closed: %s %f -> %f %s @ %f\n", u.UID, p.Direction, pp.Amount, p.Amount, p.Ticker, p.EntryPrice)
 			p.Type = PartiallyClosed
 		} else if pp.Amount < p.Amount {
 			// previously saved amount is SMALLER than the current amount
 			// meaning the amount has INCREASED thus the position
 			// has been added to
 
+			u.log.Printf("[%s] Position added to: %s %f -> %f %s @ %f\n", u.UID, p.Direction, pp.Amount, p.Amount, p.Ticker, p.EntryPrice)
 			p.Type = AddedTo
 		} else {
 			// nothing changed
